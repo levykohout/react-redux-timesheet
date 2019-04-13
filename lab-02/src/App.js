9da2b5b538a9e5114b9755986f60a11032027cf3
levykohout <levy.kohout@yahoo.com>
2019-04-13 10:47:24 -0500

    Lab 2 completed successfully

--- lab-02/src/App.js
+++ lab-02/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Projects from "./projects/Projects";
+import Projects from './projects/Projects';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div className="container">
+          <Projects />
+        </div>
       </div>
     );
   }
